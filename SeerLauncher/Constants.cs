ca167821af15c3a3c4736e4b2f98aedb1e9f203a
MengNianxiaoyao <2589141604@qq.com>
2026-08-15 20:59:16 +0800

    feat: add mvvm base, constants and config model

---
+++ SeerLauncher/Constants.cs
@@ -0,0 +1,15 @@
+namespace SeerLauncher
+{
+    public static class Constants
+    {
+        public const string UserAgent = "user-agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36";
+        public const string CheckUrl = "https://note.youdao.com/yws/api/note/9a59df393eb92eeb4daeeec5a5ed3ca2/WEB10648d40acf564333c10533de2ef8bfd?sev=j1";
+        public const string DeveloperBilibili = "https://space.bilibili.com/381745966";
+        public const string InstructionsUrl = "https://note.youdao.com/ynoteshare/index.html?id=b7228a46114ccc88324823208cea3565";
+        public const string DeveloperBlog = "https://blog.mnxyio.top/posts/seer/tools";
+        public const string YoudaoDocs = "https://note.youdao.com/ynoteshare/index.html?id=7c57edf3eab981a190d910ab47c7db97";
+        public const string StoreUrl = "https://seerxm.mnxyio.top/magic";
+        public const string CurrentVersion = "2.3.6";
+        public const string DefaultKeywords = "Seer|CHIKA|雷小伊|重聚";
+    }
+}
---
+++ SeerLauncher/Models/AppConfig.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace SeerLauncher.Models
+{
+    public class AppConfig
+    {
+        public List<string> Keywords { get; set; } = new List<string>();
+        public Dictionary<string, string> Programs { get; set; } = new Dictionary<string, string>();
+    }
+}
