2925257604a34d05dcd3b1769a611601b9c0333c
MengNianxiaoyao <2589141604@qq.com>
2026-08-17 10:09:01 +0800

    refactor: move assembly attributes from csproj to AssemblyInfo.cs

---
+++ SeerLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("赛尔号辅助启动器")]
+[assembly: AssemblyDescription("赛尔号辅助启动器")]
+[assembly: AssemblyCompany("梦念逍遥")]
+[assembly: AssemblyProduct("赛尔号辅助启动器")]
+[assembly: AssemblyCopyright("Copyright © 2026 梦念逍遥")]
+[assembly: AssemblyVersion("2.3.7.0")]
+[assembly: AssemblyFileVersion("2.3.7.0")]
+[assembly: AssemblyInformationalVersion("2.3.7")]
+[assembly: NeutralResourcesLanguage("zh-CN")]
